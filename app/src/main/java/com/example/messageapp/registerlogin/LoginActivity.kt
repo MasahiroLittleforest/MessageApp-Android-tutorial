8b1c71041a0ab09d756cb34aa40e194b0d75fa7c
github MasahiroLittleforest <masahiro.littleforest@gmail.com>
2019-06-07 23:27:38 +0900

    Done Messanger Tutorial

---
+++ app/src/main/java/com/example/messageapp/registerlogin/LoginActivity.kt
@@ -0,0 +1,53 @@
+package com.example.messageapp.registerlogin
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.example.messageapp.R
+import com.example.messageapp.messages.LatestMessagesActivity
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
+
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        login_button_login.setOnClickListener {
+            performLogin()
+        }
+
+        back_to_register_text_view.setOnClickListener {
+            finish()
+        }
+    }
+
+    private fun performLogin() {
+        val email = email_edittext_login.text.toString()
+        val password = password_edittext_login.text.toString()
+
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Please fill out email and password", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                if (!it.isSuccessful) return@addOnCompleteListener
+
+                // else if successful
+                Log.d("Login", "Successfully logged in with uid: ${it.result?.user?.uid}")
+                val intent = Intent(this, LatestMessagesActivity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(intent)
+            }
+            .addOnFailureListener {
+                Log.d("Login", "Failed to log in: ${it.message}")
+                Toast.makeText(this, "Failed to log in: ${it.message}", Toast.LENGTH_SHORT).show()
+            }
+    }
+}
